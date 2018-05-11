2ce1352ee808dcc110e253e1080c9726af910f99
Jackson W Pluskota <jacksonw7007@hotmail.com>
2018-05-11 14:13:09 -0300

    spaceShip and rotation moon

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -91,33 +91,34 @@ void ofApp::mousePressed(int x, int y, int button)
 {
     spaceship->clickMouse(x, y);
     clicked = true;
+}
 
-    //--------------------------------------------------------------
-    void ofApp::mouseReleased(int x, int y, int button)
-    {
-    }
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button)
+{
+}
 
-    //--------------------------------------------------------------
-    void ofApp::mouseEntered(int x, int y)
-    {
-    }
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y)
+{
+}
 
-    //--------------------------------------------------------------
-    void ofApp::mouseExited(int x, int y)
-    {
-    }
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y)
+{
+}
 
-    //--------------------------------------------------------------
-    void ofApp::windowResized(int w, int h)
-    {
-    }
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h)
+{
+}
 
-    //--------------------------------------------------------------
-    void ofApp::gotMessage(ofMessage msg)
-    {
-    }
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg)
+{
+}
 
-    //--------------------------------------------------------------
-    void ofApp::dragEvent(ofDragInfo dragInfo)
-    {
-    }
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
+}
